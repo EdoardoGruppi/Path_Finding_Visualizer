4ee45b0c89457fac8759a5e48f4e83bd3f2b6e85
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-09-07 11:57:54 +0200

    🔨 Add Greedy algorithm

---
+++ src/Algorithms/Greedy.js
@@ -0,0 +1,42 @@
+import {
+  getUnvisitedNeighbors,
+  heuristic,
+  getAllNodes,
+  sortNodesByDistanceToEnd,
+} from "./Utils";
+
+export function greedy(grid, startNode, finishNode) {
+  const visitedNodesInOrder = [];
+  // Except for the starting node the distance == Infinity
+  startNode.heuristic = 0;
+  // At the beginning all the nodes are unvisited
+  const unvisitedNodes = getAllNodes(grid);
+  // While there are still unvisited nodes
+  while (unvisitedNodes.length) {
+    // Sort nodes by distance
+    sortNodesByDistanceToEnd(unvisitedNodes);
+    // Take from the array the first element, i.e. the nearest
+    const closestNode = unvisitedNodes.shift();
+    // If it corresponds to a wall, it is skipped
+    if (closestNode.isWall) continue;
+    // If the closest node is at a distance of infinity, the game is trapped and must be stopped
+    if (closestNode.heuristic === Infinity) return visitedNodesInOrder;
+    // Otherwise the node is traversed by the algorithm
+    closestNode.isVisited = true;
+    visitedNodesInOrder.push(closestNode);
+    // If the node is the target the objective is accomplished
+    if (closestNode === finishNode) return visitedNodesInOrder;
+    // Update distances
+    updateUnvisitedNeighbors(closestNode, grid, finishNode);
+  }
+}
+
+function updateUnvisitedNeighbors(node, grid, finishNode) {
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+  for (const neighbor of unvisitedNeighbors) {
+    const distanceToEnd = heuristic(neighbor, finishNode);
+    if (neighbor.heuristic > distanceToEnd) neighbor.heuristic = distanceToEnd;
+    // Link each neighbour to the previous node
+    neighbor.previousNode = node;
+  }
+}
